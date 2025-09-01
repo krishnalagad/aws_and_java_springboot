aa29f4d0cb8d1c4b17a596ce53fea5395fd17036
Krishna Lagad <krishnalagad2@gmail.com>
2025-09-01 19:25:35 +0530

    added empty project for ec2

---
+++ AWSEC2Demo/src/main/java/com/sample/AWSEC2Demo/Awsec2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.sample.AWSEC2Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Awsec2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Awsec2DemoApplication.class, args);
+	}
+
+}
---
+++ AWSEC2Demo/src/test/java/com/sample/AWSEC2Demo/Awsec2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.AWSEC2Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Awsec2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

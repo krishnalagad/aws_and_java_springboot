8a478c3b092b6e7eb75f708742ccfb7fd70984f6
Krishna Lagad <krishnalagad2@gmail.com>
2025-09-01 19:53:03 +0530

    added dummy api for testing purpose

--- AWSEC2Demo/src/main/java/com/sample/AWSEC2Demo/Awsec2DemoApplication.java
+++ AWSEC2Demo/src/main/java/com/sample/AWSEC2Demo/Awsec2DemoApplication.java
@@ -2,12 +2,23 @@ package com.sample.AWSEC2Demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Awsec2DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Awsec2DemoApplication.class, args);
+	}
+
+	@GetMapping("/api/{str}")
+	public ResponseEntity<String> dummy(@PathVariable String str) {
+		return ResponseEntity.ok("Hello " + str + " !! Welcome to the AWS EC2");
 	}
 
 }
